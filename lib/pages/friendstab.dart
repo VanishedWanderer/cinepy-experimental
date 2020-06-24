0ddc0191bf76cde82184fc2c9130d3dd3293b791
JanNeuburger <jan.neuburger.jn@gmail.com>
2020-06-24 12:48:15 +0200

    Added Files for Tabs

---
+++ lib/pages/friendstab.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FriendsPage extends StatefulWidget{
+  @override
+  _FriendsPageState createState() => _FriendsPageState();
+
+}
+
+class _FriendsPageState extends State<FriendsPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Helo'),
+      ),
+    );
+  }
+
+}
---
+++ lib/pages/moviestab.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/cupertino.dart';
+
+class MoviesPage extends StatefulWidget{
+  @override
+  _MoviesPageState createState() => _MoviesPageState();
+
+}
+
+class _MoviesPageState extends State<MoviesPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Ser ar muvies!'),
+    );
+  }
+
+}
