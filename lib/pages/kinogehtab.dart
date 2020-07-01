a041e52daf38f132f18950b4bb00f64e14c287db
JanNeuburger <jan.neuburger.jn@gmail.com>
2020-07-01 10:22:24 +0200

    KinogehTab now uses a ListViewBuilder

--- lib/pages/kinogehtab.dart
+++ lib/pages/kinogehtab.dart
@@ -9,9 +9,37 @@ class KinogehPage extends StatefulWidget{
 }
 
 class _KinogehPageState extends State<KinogehPage>{
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Text('Gemma Kino');
+    final List<String> items = <String>[
+      'The Lighthouse', 'Parasite', 'Knives Out', 'Joker', 'Once Upon a Time... in Hollywood', 'John Wick'
+    ];
+    return ListView.builder(itemBuilder: (BuildContext context, int index){
+      return Container(
+        child: Card(
+          margin: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.movie_filter),
+                title: Text('${items[index]}'),
+              ),
+              ButtonBar(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+      }, itemCount: items.length, primary: true,);
   }
 
 }
