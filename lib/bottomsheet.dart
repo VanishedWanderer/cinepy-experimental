df514d38892404bb4c3f95b47ee6ff0315173e73
JanNeuburger <jan.neuburger.jn@gmail.com>
2020-07-01 09:43:02 +0200

    Added About Dialog

--- lib/bottomsheet.dart
+++ lib/bottomsheet.dart
@@ -11,7 +11,7 @@ class _BottomNavState extends State<BottomNav>{
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
+      height: 250,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.only(topLeft: Radius.circular(16.0), topRight: Radius.circular(16.0))
       ),
@@ -19,15 +19,21 @@ class _BottomNavState extends State<BottomNav>{
         padding: EdgeInsets.all(8),
         children: <Widget>[
           ListTile(
-            title: Text('Profile'),
+            title: Text('Profil'),
             leading: Icon(Icons.person),
           onTap: () {},
           ),
           const Divider(),
           ListTile(
-            title: Text('Settings'),
+            title: Text('Einstellungen'),
             leading: Icon(Icons.settings),
           onTap: () {},
+          ),
+          const Divider(),
+          ListTile(
+            title: Text('Über die App'),
+            leading: Icon(Icons.info_outline),
+            onTap: () { showAboutDialog(context: context); },
           )
         ],
       )
