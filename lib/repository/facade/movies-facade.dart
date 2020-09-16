e265a58c1b9d1c0d4d1d76c99991708986d79131
Erik Mayrhofer <obyoxar@gmail.com>
2020-09-16 17:45:39 +0200

    movies page with refresher

--- lib/repository/facade/movies-facade.dart
+++ lib/repository/facade/movies-facade.dart
@@ -2,9 +2,13 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gemmakino/repository/BackendAdapter.dart';
 import 'package:gemmakino/repository/model/movie.dart';
 import 'package:gemmakino/util/collection-util.dart';
+import 'package:graphql/client.dart';
 import 'package:mobx/mobx.dart';
+import 'package:provider/provider.dart';
 
 part 'movies-facade.g.dart'; // flutter pub run build_runner build
 
@@ -12,28 +16,51 @@ abstract class MoviesFacade = _MoviesFacade with _$MoviesFacade;
 
 
 abstract class _MoviesFacade with Store {
-  @observable
   ObservableList<Movie> movies;
 
-  void queryAll();
+  Future<List<Movie>> queryAll();
 }
 
 
+class MoviesGraphQlFacade extends MoviesFacade {
+  @override
+  Future<List<Movie>> queryAll() async{
+    final value = await BackendAdapter().client.query(QueryOptions(
+      documentNode: gql(r'''
+      query {
+        movies {
+          name
+        }
+      }
+      '''),
+      fetchPolicy: FetchPolicy.networkOnly
 
+    ));
+    if(value.hasException){
+      throw value.exception;
+    }else{
+      final rawMovies = value.data['movies'] as List<dynamic>;
+      print(rawMovies);
+      final newMovies = rawMovies.map((dynamic e) => Movie(id: 0, name: e['name'] as String)).toList();
+      movies = ObservableList.of(newMovies);
+      return newMovies;
+    }
+  }
+}
 
 class MoviesMockFacade extends MoviesFacade {
   @override
-  void queryAll() {
-    Future<void>.delayed(const Duration(milliseconds: 500)).then((value) {
-        movies = ObservableList.of([
-          'The Lighthouse', 'Parasite', 'Knives Out', 'Joker', 'Once Upon a Time... in Hollywood', 'John Wick'
-        ].mapIndexed((index, item) => Movie(id: index, name: item)).toList());
-    });
-
+  Future<List<Movie>> queryAll() async {
+    await Future<void>.delayed(const Duration(milliseconds: 500));
+    final list = [
+      'The Lighthouse', 'Parasite', 'Knives Out', 'Joker', 'Once Upon a Time... in Hollywood', 'John Wick'
+    ].mapIndexed((index, item) => Movie(id: index, name: item)).toList();
+    movies = ObservableList.of(list);
     final random = Random();
     
     Timer.periodic(const Duration(seconds: 5), (_) {
       movies[random.nextInt(movies.length)].name += 'a';
     });
+    return list;
   }
 }
