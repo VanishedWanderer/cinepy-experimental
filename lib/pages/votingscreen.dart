1e3d2092b88d28d5cd1349c34b0d1bb677e0173e
Erik Mayrhofer <obyoxar@gmail.com>
2020-09-23 12:12:00 +0200

    implement dismissible

--- lib/pages/votingscreen.dart
+++ lib/pages/votingscreen.dart
@@ -12,6 +12,9 @@ class VotingScreen extends StatefulWidget {
 }
 
 class _VotingScreenState extends State<VotingScreen> {
+
+  final List<String> items = '012301230123012301230'.characters.map((e) => 'Item $e').toList(growable: true);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,14 +24,38 @@ class _VotingScreenState extends State<VotingScreen> {
             pinned: false,
             floating: true,
             snap: false,
+            flexibleSpace: TabBar(
+              tabs: [
+                Text('O')
+              ],
+            ),
             title: Hero(
               tag: widget.text,
               child: Text('Date for Kinogeh löl')
             ),
           ),
           SliverList(delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-            return ListTile(title: Text('YoinkYeet $index'));
-          }, childCount: 40))
+            return Dismissible(
+                key: Key(items[index]),
+                secondaryBackground: Container(color: Colors.red),
+                background: Container(color: Colors.green),
+                onDismissed: (direction) {
+                  setState(() {
+                    items.removeAt(index);
+                  });
+                  if(direction == DismissDirection.startToEnd){
+                    Scaffold.of(context).showSnackBar(
+                      SnackBar(content: Text('Item ${items[index]} accepted.'))
+                    );
+                  }else if(direction == DismissDirection.endToStart){
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Item ${items[index]} declined.'))
+                    );
+                  }
+                },
+                child: ListTile(title: Text('YoinkYeet ${items[index]}'))
+            );
+          }, childCount: items.length))
         ],
       ),
     );
