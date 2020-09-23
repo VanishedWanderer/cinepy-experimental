bf71d21fe36f7c4076c6856aa12e9cad81fe5f28
Erik Mayrhofer <obyoxar@gmail.com>
2020-09-23 13:09:26 +0200

    add voting screen tabs

--- lib/pages/votingscreen.dart
+++ lib/pages/votingscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 class VotingScreen extends StatefulWidget {
@@ -11,53 +12,106 @@ class VotingScreen extends StatefulWidget {
   final String text;
 }
 
-class _VotingScreenState extends State<VotingScreen> {
+class _VotingScreenState extends State<VotingScreen>
+    with SingleTickerProviderStateMixin {
+  final List<String> items = '012301230123012301230'
+      .characters
+      .map((e) => 'Item $e')
+      .toList(growable: true);
 
-  final List<String> items = '012301230123012301230'.characters.map((e) => 'Item $e').toList(growable: true);
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            pinned: false,
-            floating: true,
-            snap: false,
-            flexibleSpace: TabBar(
-              tabs: [
-                Text('O')
-              ],
+      body: NestedScrollView(
+        headerSliverBuilder: (context, value) {
+          return [
+            SliverAppBar(
+              pinned: true,
+              floating: true,
+              snap: true,
+              title: Hero(tag: widget.text, child: Text('Date for Kinogeh löl')),
+              bottom: TabBar(
+                controller: controller,
+                tabs: const [
+                  Tab(text: 'Oink'),
+                  Tab(text: 'Boink'),
+                  Tab(text: 'Zoink'),
+                ],
+              ),
+              flexibleSpace: const FlexibleSpaceBar(
+                collapseMode: CollapseMode.pin,
+              ),
+            )
+          ];
+        },
+        body: TabBarView(
+          controller: controller,
+          children: [
+            Container(
+              child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                        key: Key(items[index]),
+                        secondaryBackground: Container(color: Colors.red),
+                        background: Container(color: Colors.green),
+                        onDismissed: (direction) {
+                          setState(() {
+                            items.removeAt(index);
+                          });
+                          if (direction == DismissDirection.startToEnd) {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content:
+                                    Text('Item ${items[index]} accepted.')));
+                          } else if (direction == DismissDirection.endToStart) {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                                content:
+                                    Text('Item ${items[index]} declined.')));
+                          }
+                        },
+                        child:
+                            ListTile(title: Text('YoinkYeet ${items[index]}')));
+                  },
+                  itemCount: items.length),
             ),
-            title: Hero(
-              tag: widget.text,
-              child: Text('Date for Kinogeh löl')
-            ),
-          ),
-          SliverList(delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-            return Dismissible(
-                key: Key(items[index]),
-                secondaryBackground: Container(color: Colors.red),
-                background: Container(color: Colors.green),
-                onDismissed: (direction) {
-                  setState(() {
-                    items.removeAt(index);
-                  });
-                  if(direction == DismissDirection.startToEnd){
-                    Scaffold.of(context).showSnackBar(
-                      SnackBar(content: Text('Item ${items[index]} accepted.'))
-                    );
-                  }else if(direction == DismissDirection.endToStart){
-                    Scaffold.of(context).showSnackBar(
-                        SnackBar(content: Text('Item ${items[index]} declined.'))
-                    );
-                  }
-                },
-                child: ListTile(title: Text('YoinkYeet ${items[index]}'))
-            );
-          }, childCount: items.length))
-        ],
+            Text('Hi'),
+            Text('Hi'),
+          ],
+        ),
       ),
     );
+  }
+}
+
+class _TabSliverPersistentHeaderDelegate
+    extends SliverPersistentHeaderDelegate {
+  const _TabSliverPersistentHeaderDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height + 20;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height + 20;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
