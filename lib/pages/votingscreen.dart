a2a0dbbff221c2eba4f9e5d49b18f70e55c6a7fb
Erik Mayrhofer <obyoxar@gmail.com>
2020-09-30 15:15:26 +0200

    work on voting-screen

--- lib/pages/votingscreen.dart
+++ lib/pages/votingscreen.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -28,6 +29,14 @@ class Question {
   }
 }
 
+class Group {
+  Group(this.title, this.candidates);
+
+  String title;
+  bool expanded = false;
+  List<Candidate> candidates;
+}
+
 class _VotingScreenState extends State<VotingScreen>
     with SingleTickerProviderStateMixin {
   static final List<String> _cinemas = [
@@ -35,35 +44,30 @@ class _VotingScreenState extends State<VotingScreen>
     "WongoSchwongoCinema",
     "Lölchenplexx"
   ];
+
   final List<Candidate> items = 'abcdefghjiklmnopqrstuvwxyz'
       .characters
-      .map((e) => Candidate(Movie(name: "Movie $e"), DateTime.now(),
-      _cinemas[Random().nextInt(_cinemas.length)]))
+      .map((e) => Candidate(
+          Movie(name: "Movie $e"),
+          DateTime.now().add(Duration(days: Random().nextInt(5))),
+          _cinemas[Random().nextInt(_cinemas.length)]))
       .toList(growable: false);
 
   final List<Candidate> acceptedItems = [];
   final List<Candidate> declinedItems = [];
 
-  final List<Question> questions = _cinemas.map((e) {
-    return Question("Would you go to cinema ${e}", (c, s) => !(c.cinema == e));
-  }).toList(growable: true);
-
-  void accept(List<Candidate> list, int index) {
-    acceptedItems.add(items.removeAt(index));
-  }
-
-  void decline(Candidate c){
-    declinedItems.add(items.removeAt(index));
-  }
-
-  final List<Question> answeredQuestions = [];
-
   TabController controller;
+
+  List<Group> groups;
 
   @override
   void initState() {
     super.initState();
     controller = TabController(length: 3, vsync: this);
+    groups = groupBy(items, (Candidate val) => val.cinema)
+        .entries
+        .map((e) => Group(e.key, e.value))
+        .toList();
   }
 
   @override
@@ -76,8 +80,8 @@ class _VotingScreenState extends State<VotingScreen>
               pinned: true,
               floating: true,
               snap: true,
-              title: Hero(
-                  tag: widget.text, child: Text('Date for Kinogeh löl')),
+              title:
+                  Hero(tag: widget.text, child: Text('Date for Kinogeh löl')),
               bottom: TabBar(
                 controller: controller,
                 tabs: const [
@@ -89,81 +93,88 @@ class _VotingScreenState extends State<VotingScreen>
               flexibleSpace: const FlexibleSpaceBar(
                 collapseMode: CollapseMode.pin,
                 background: Padding(
-                  padding: EdgeInsets.only(top: 100), child: Text('sweet'),),
+                  padding: EdgeInsets.only(top: 100),
+                  child: Text('sweet'),
+                ),
               ),
             ),
-
-
           ];
         },
         body: TabBarView(
           controller: controller,
           children: [
             Container(
-              child: CustomScrollView(
-                  slivers: [
-                    SliverList(delegate: SliverChildBuilderDelegate((
-                        BuildContext context, int index) {
-                      return Dismissible(
-                          key: Key(questions[index].text),
-                          secondaryBackground: Container(color: Colors.red),
-                          background: Container(color: Colors.green),
-                          child: ListTile(
-                            title: Text(questions[index].text),
-                          ),
-                          onDismissed: (direction) {
-                            setState(() {
-                              if (direction == DismissDirection.startToEnd) {
-                                questions[index].status = Status.ACCEPTED;
-                              } else {
-                                questions[index].status = Status.DECLINED;
-                              }
-                              answeredQuestions.add(questions.removeAt(index));
-                            });
-                          },
-                      );
-                    }, childCount: questions.length)),
-
-                    SliverList(delegate: SliverChildBuilderDelegate((
-                        BuildContext context, int index) {
-                      return Dismissible(
-                          key: Key(items[index].hashCode.toString()),
-                          secondaryBackground: Container(color: Colors.red),
-                          background: Container(color: Colors.green),
-                          onDismissed: (direction) {
-                            if (direction == DismissDirection.startToEnd) {
-                              setState(() {
-                                items[index].status = Status.ACCEPTED;
-                              });
-                              Scaffold.of(context).showSnackBar(SnackBar(
-                                  content:
-                                  Text('Item ${items[index]} accepted.')));
-                            } else
-                            if (direction == DismissDirection.endToStart) {
-                              setState(() {
-                                items[index].status = Status.DECLINED;
-                              });
-                              Scaffold.of(context).showSnackBar(SnackBar(
-                                  content:
-                                  Text('Item ${items[index]} declined.')));
-                            }
-                          },
-                          child:
-                          ListTile(title: Text('${items[index].movie
-                              .name} played in ${items[index]
-                              .cinema} on ${items[index].time.toLocal()}')));
-                    }, childCount: items.length))
-                  ]
-              ),
+                child: SingleChildScrollView(
+                  child: ExpansionPanelList(
+                      expansionCallback: (int index, bool expanded) {
+                        setState(() {
+                          groups[index].expanded = !expanded;
+                        });
+                      },
+                      children: groups.map((e) {
+                        return ExpansionPanel(
+                            headerBuilder:
+                                (BuildContext context, bool isExpanded) {
+                              return Dismissible(
+                                key: Key("DismissGroup ${e.title}"),
+                                background: Container(color: Colors.green),
+                                child: ListTile(title: Text(e.title)),
+                                onDismissed: (direction) {
+                                  setState(() {
+                                    groups.remove(e);
+                                  });
+                                },
+                              );
+                            },
+                            body: Column(
+                                children: e.candidates.map((candidate) {
+                              return Dismissible(
+                                key: Key("${candidate.movie.name}"),
+                                child:
+                                    ListTile(title: Text(candidate.movie.name)),
+                                onDismissed: (direction) {
+                                  setState(() {
+                                    e.candidates.remove(candidate);
+                                  });
+                                },
+                              );
+                            }).toList()),
+                            isExpanded: e.expanded);
+                      }).toList()),
+                )
             ),
-
-
-
+            Text('Hi'),
             Text('Hi'),
           ],
         ),
       ),
     );
+  }
+}
+
+class _ChipsSliverPersistentHeaderDelegate
+    extends SliverPersistentHeaderDelegate {
+  const _ChipsSliverPersistentHeaderDelegate(this._listView);
+
+  final ListView _listView;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _listView,
+    );
+  }
+
+  @override
+  double get maxExtent => 60;
+
+  @override
+  double get minExtent => 60;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
   }
 }
 
@@ -174,8 +185,8 @@ class _TabSliverPersistentHeaderDelegate
   final TabBar _tabBar;
 
   @override
-  Widget build(BuildContext context, double shrinkOffset,
-      bool overlapsContent) {
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
     return Container(
       child: _tabBar,
     );
